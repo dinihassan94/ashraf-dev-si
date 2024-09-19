30c2ec51bd5b4ec1914c727b69879a72388f943c
Dini Hassan Youssouf <47700@eddj.local>
2024-09-19 11:33:47 +0300

    encore-ashraf

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,263 @@
+dmx.config({
+  "manuelle": {
+    "fastscan_result": {
+      "meta": [
+        {
+          "name": "num_dossier",
+          "type": "number"
+        },
+        {
+          "name": "id_client",
+          "type": "number"
+        },
+        {
+          "name": "nom_client",
+          "type": "text"
+        },
+        {
+          "name": "adresse",
+          "type": "text"
+        },
+        {
+          "name": "nombre_compteur",
+          "type": "number"
+        },
+        {
+          "name": "support_proche",
+          "type": "text"
+        },
+        {
+          "name": "lib_long",
+          "type": "text"
+        },
+        {
+          "name": "user_crea",
+          "type": "number"
+        },
+        {
+          "name": "lib_court",
+          "type": "text"
+        },
+        {
+          "name": "date_crea",
+          "type": "datetime"
+        },
+        {
+          "name": "pl",
+          "type": "text"
+        },
+        {
+          "name": "contrat_en_cours",
+          "type": "number"
+        },
+        {
+          "name": "nom_cellule_respo",
+          "type": "text"
+        },
+        {
+          "name": "n_devis",
+          "type": "text"
+        },
+        {
+          "name": "ps",
+          "type": "text"
+        },
+        {
+          "name": "id_statut",
+          "type": "number"
+        },
+        {
+          "name": "num_cellule_respo",
+          "type": "number"
+        },
+        {
+          "name": "type_clientele",
+          "type": "number"
+        },
+        {
+          "name": "rm1",
+          "type": "number"
+        },
+        {
+          "name": "intervention_dmd",
+          "type": "number"
+        }
+      ],
+      "outputType": "text"
+    },
+    "tableRepeat1": {
+      "meta": [
+        {
+          "name": "$index",
+          "type": "number"
+        },
+        {
+          "name": "$key",
+          "type": "text"
+        },
+        {
+          "name": "$value",
+          "type": "object"
+        },
+        {
+          "name": "num_dossier",
+          "type": "number"
+        },
+        {
+          "name": "id_client",
+          "type": "number"
+        },
+        {
+          "name": "nom_client",
+          "type": "text"
+        },
+        {
+          "name": "adresse",
+          "type": "text"
+        },
+        {
+          "name": "nombre_compteur",
+          "type": "number"
+        },
+        {
+          "name": "support_proche",
+          "type": "text"
+        },
+        {
+          "name": "lib_long",
+          "type": "text"
+        },
+        {
+          "name": "user_crea",
+          "type": "number"
+        },
+        {
+          "name": "lib_court",
+          "type": "text"
+        },
+        {
+          "name": "date_crea",
+          "type": "datetime"
+        },
+        {
+          "name": "pl",
+          "type": "text"
+        },
+        {
+          "name": "contrat_en_cours",
+          "type": "number"
+        },
+        {
+          "name": "nom_cellule_respo",
+          "type": "text"
+        },
+        {
+          "name": "n_devis",
+          "type": "text"
+        },
+        {
+          "name": "ps",
+          "type": "text"
+        },
+        {
+          "name": "id_statut",
+          "type": "number"
+        },
+        {
+          "name": "num_cellule_respo",
+          "type": "number"
+        },
+        {
+          "name": "type_clientele",
+          "type": "number"
+        },
+        {
+          "name": "rm1",
+          "type": "number"
+        },
+        {
+          "name": "intervention_dmd",
+          "type": "number"
+        }
+      ],
+      "outputType": "text"
+    },
+    "cMontant": {
+      "meta": null,
+      "outputType": "text"
+    },
+    "devisListe": {
+      "meta": [
+        {
+          "name": "libelle",
+          "type": "text"
+        },
+        {
+          "name": "quantite",
+          "type": "number"
+        },
+        {
+          "name": "prix_unitaire",
+          "type": "number"
+        },
+        {
+          "name": "num_ligne_tarifaire",
+          "type": "number"
+        },
+        {
+          "name": "montant",
+          "type": "number"
+        },
+        {
+          "name": "total",
+          "type": "number"
+        },
+        {
+          "name": "num_ano",
+          "type": "number"
+        },
+        {
+          "name": "unite",
+          "type": "text"
+        },
+        {
+          "name": "facture",
+          "type": "text"
+        },
+        {
+          "name": "ligne_tarif_parent",
+          "type": "number"
+        },
+        {
+          "name": "code_article",
+          "type": "number"
+        },
+        {
+          "name": "id_contrat",
+          "type": "number"
+        },
+        {
+          "name": "id_statut",
+          "type": "number"
+        },
+        {
+          "name": "num_depannage",
+          "type": "number"
+        },
+        {
+          "name": "localisation",
+          "type": "text"
+        },
+        {
+          "name": "num_contrat",
+          "type": "number"
+        },
+        {
+          "name": "statut_devis",
+          "type": "number"
+        }
+      ],
+      "outputType": "text"
+    }
+  }
+});
